79134869853326ebeeabb2f859e69685570dac09
AbrarNitk <abrar.nitk@gmail.com>
2019-11-04 23:05:52 +0530

    add static api

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,9 @@ extern crate emi_calc;
 #[macro_use]
 extern crate serde;
 
+use actix_web::http::header::ContentType;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use emi_calc::emi;
-use std::fs;
-use std::io;
 use std::io::prelude::*;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -14,24 +13,13 @@ pub struct CalculateEmi {
     rate: f64,
     tenure: i32,
 }
-
+// <script src="/static/script.js" type="text/javascript"></script>
 fn index() -> impl Responder {
-    HttpResponse::Ok().body(
-        r#"
-        <html>
-            <head>
-                <script src="/static/script.js" type="text/javascript"></script>
-            </head>
-            <body>
-                <h1>Hellow</h1>
-            </body>
-        </html>
-    "#,
-    )
+    let index_page = static_content("emi/build/index.html");
+    HttpResponse::Ok().set(ContentType::html()).body(index_page)
 }
 
 fn static_content(path: &str) -> Vec<u8> {
-    let path = format!("static/{}", path);
     let source_path = std::path::Path::new(&path);
     let mut src = std::fs::File::open(&source_path).unwrap();
     let mut content = Vec::new();
@@ -39,8 +27,30 @@ fn static_content(path: &str) -> Vec<u8> {
     content
 }
 
-fn serve_static(path: web::Path<String>) -> impl Responder {
+fn serve_js_static(path: web::Path<String>) -> impl Responder {
+    let path = format!("emi/build/static/js/{}", path);
+    println!("{}", path);
     HttpResponse::Ok().body(static_content(&path))
+}
+
+fn serve_css_static(path: web::Path<String>) -> impl Responder {
+    let path = format!("emi/build/static/css/{}", path);
+    println!("{}", path);
+    HttpResponse::Ok()
+        .set_header("Accept", "text/css,*/*;q=0.")
+        .set_header("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8")
+        .set_header("Accept-Encoding", "gzip")
+        .body(static_content(&path))
+}
+
+fn serve_media_static(path: web::Path<String>) -> impl Responder {
+    let path = format!("emi/build/static/media/{}", path);
+    println!("{}", path);
+    HttpResponse::Ok()
+        .set_header("Content-Type", "image/svg+xml; charset=UTF-8")
+        .set_header("Accept-Ranges", "bytes")
+        .set_header("Accept-Encoding", "gzip")
+        .body(static_content(&path))
 }
 
 fn calculate_emi(data: web::Json<CalculateEmi>) -> impl Responder {
@@ -53,7 +63,9 @@ fn main() {
         App::new()
             .route("/", web::get().to(index))
             .route("/calculate_emi", web::post().to(calculate_emi))
-            .route("/static/{path}", web::get().to(serve_static))
+            .route("/static/css/{path}", web::get().to(serve_css_static))
+            .route("/static/js/{path}", web::get().to(serve_js_static))
+            .route("/static/media/{path}", web::get().to(serve_media_static))
     })
     .bind("127.0.0.1:8080")
     .unwrap()
