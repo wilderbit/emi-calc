db03616986955867700e413274cdca830b6a11fa
AbrarNitk <abrar.nitk@gmail.com>
2019-11-10 18:13:44 +0530

    handle form errors

--- emi/src/index.js
+++ emi/src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function FieldError(props) {
+    return(<div className="error">{props.error}</div>)
+}
+
 class EMIForm extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            principal: 0,
-            rate: 0,
-            months: 0,
+            showPrincipalError: [],
+            showRateError: [],
+            showMonthsError: [],
+            principal: "",
+            rate: "",
+            months: "",
             success: false,
             data: null,
             error: null,
@@ -21,16 +28,85 @@ class EMIForm extends React.Component {
     changeHandler =  (event) => {
         const name = event.target.name;
         const value = event.target.value;
-
         this.setState({
             [name]: value
         })
     };
 
-    handleSubmit(event) {
-        console.log("Reached::: ", this.state.principal);
+    inputOnClick = () => {
+        this.setState({
+            showPrincipalError: [],
+            showRateError: [],
+            showMonthsError: [],
+        });
+    };
 
+
+
+    genError = (value) => {
+
+    };
+
+    handleSubmit(event) {
         event.preventDefault();
+        const principal = this.state.principal;
+        const rate = this.state.rate;
+        const months = this.state.months;
+
+        if (principal === "" || isNaN(principal)) {
+            this.setState({
+                showPrincipalError: [<FieldError error="Please enter a valid principal amount" />]
+            });
+            return;
+        }
+
+        if (rate === "" || isNaN(rate)) {
+            this.setState({
+                showRateError: [<FieldError error="Please enter a valid rate of interest" />]
+            });
+            return;
+        }
+
+        if (months === "" || isNaN(months)) {
+            this.setState({
+                showMonthsError: [<FieldError error="Please enter a valid number of months" />]
+            });
+            return;
+        }
+
+        const months_int = parseInt(months, 10);
+        const amount = Number.parseFloat(principal);
+        const interest = Number.parseFloat(rate);
+
+        if (!Number.isInteger(months_int)) {
+            this.setState({
+                showMonthsError: [<FieldError error="Please enter months as valid integer" />]
+            });
+            return;
+        }
+
+        if( amount <= 0 ) {
+            this.setState({
+                showPrincipalError: [<FieldError error="Please enter principal greater than zero" />]
+            });
+            return;
+        }
+
+        if( interest <= 0 ) {
+            this.setState({
+                showRateError: [<FieldError error="Interest rate should be greater than zero" />]
+            });
+            return;
+        }
+
+        if( months_int <= 0 || months_int > 1200) {
+            this.setState({
+                showMonthsError: [<FieldError error="Months should be in range between 1 and 1200" />]
+            });
+            return;
+        }
+
+
         fetch("http://127.0.0.1:8080/calculate_emi/", {
             method: 'POST',
             headers: {
@@ -38,9 +114,9 @@ class EMIForm extends React.Component {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                "amount": 10000000,
-                "rate": 8.5,
-                "tenure": 12
+                "amount": amount,
+                "rate": interest,
+                "tenure": months_int,
             }),
         }
         ).then(res => res.json())
@@ -59,18 +135,22 @@ class EMIForm extends React.Component {
                     isLoaded: true,
                 })
             }
-        )
+        );
     }
+
     render() {
         return (
             <div>
-                <form className="form" onSubmit={this.handleSubmit}>
+                <form className="form" onSubmit={this.handleSubmit} method="POST">
                     <label className="label">Principal Amount:</label>
-                    <input className="input" type="text" name="principal" value={this.state.principal} onChange={this.changeHandler}/>
+                    <input className="input" type="text" name="principal" value={this.state.principal} onChange={this.changeHandler} onClick={this.inputOnClick}/>
+                    <div id="principal_error">{this.state.showPrincipalError}</div>
                     <label className="label">Rate of Interest:</label>
-                    <input className="input" type="text" name="rate" value={this.state.rate} onChange={this.changeHandler}/>
+                    <input className="input" type="text" name="rate" value={this.state.rate} onChange={this.changeHandler} onClick={this.inputOnClick}/>
+                    <div id="rate_error">{this.state.showRateError}</div>
                     <label className="label">Number of Months:</label>
-                    <input className="input" type="text" name="months" value={this.state.months} onChange={this.changeHandler}/>
+                    <input className="input" type="text" name="months" value={this.state.months} onChange={this.changeHandler} onClick={this.inputOnClick}/>
+                    <div id="months_error">{this.state.showMonthsError}</div>
                     <input className="input" type="submit" value="Submit"/>
                 </form>
                 <Table state={this.state}/>
